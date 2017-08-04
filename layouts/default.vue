ab494856b5c292333c1aac4a56b445a1793f917c
taka-sho <takasho530000@gmail.com>
2017-08-04 23:13:41 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div
+    md-toolbar
+      md-button.md-icon-button
+      h2.md-title Hiyoshi Festival 2017
+    nuxt
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  header {
+    top: 0;
+    left: 0;
+    height: 40px;
+    width: calc(100% - 40px);
+    margin: 0;
+    padding: 0 20px;
+    background-color: rgba(100, 100, 100, 0.8);
+
+    h1 {
+      font-size: 25px;
+    }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang='pug'>
+  md-layout(md-gutter)
+    md-layout.pages(md-flex-medium='50' md-flex-large='100')
+      md-tabs(md-fixed)
+        md-tab(md-label='day1')
+          h2 Day1
+        md-tab(md-label='day2')
+          h2 Day2
+    md-layout(md-flex-medium='50')
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='scss'>
+.pages {
+  margin-top: 50px;
+}
+</style>
